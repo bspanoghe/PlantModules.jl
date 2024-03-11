718ee1258856b2583eccec48be917b78bfcda239
bspanoghe <bram.spanoghe@gmail.com>
2024-03-11 16:31:26 +0100

    Update runtutorials.jl

--- tutorials/runtutorials.jl
+++ tutorials/runtutorials.jl
@@ -1,7 +1,7 @@
 if split(pwd(), '\\')[end] != "tutorials"
     cd("./tutorials")
 end
-using Pkg; Pkg.instantiate("."); Pkg.activate(".")
+using Pkg; Pkg.activate(".")
 Pkg.develop(path = "..")
 
 using Pluto
