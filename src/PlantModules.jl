9089521de7f64bac5478d341ff15175698c7bcfa
Bram Spanoghe <70662421+bspanoghe@users.noreply.github.com>
2025-09-12 17:43:34 +0200

    smoothing functions in own file

--- src/PlantModules.jl
+++ src/PlantModules.jl
@@ -1,26 +1,32 @@
 module PlantModules
 
-export Shape, Sphere, Cylinder, Cuboid
-export volume, cross_area, surface_area
-export generate_system, PlantStructure, PlantFunctionality
-export hydraulic_module, constant_carbon_module, environmental_module, Ψ_soil_module, Ψ_air_module
-export K_module, constant_K_module, hydraulic_connection, const_hydraulic_connection, evaporation_connection
-export readXEG, convert_to_MTG, convert_to_PG
-export plotgraph, plotnode
+# # Exports
+export generate_system, PlantStructure, PlantFunctionality # generate system
+export Shape, Sphere, Cylinder, Cuboid # shapes
+export getdimensionality, volume, cross_area, surface_area # shape functions
+export logsumexp, smooth_daynight # smooth functions
+export hydraulic_module, environmental_module, constant_carbon_module, simple_photosynthesis_module, Ψ_soil_module, Ψ_air_module # node modules
+export K_module, constant_K_module, hydraulic_connection, const_hydraulic_connection, evaporation_connection # edge modules
+export readXEG, convert_to_MTG, convert_to_PG # graph reading and converting
+export plotgraph, plotnode # plotting
 
-using SciMLBase # Interface with ODE solver packages
-using ModelingToolkit, Unitful # Simulation
-import ModelingToolkit: get_eqs, get_unknowns, get_ps, get_parameter_dependencies, get_observed, get_continuous_events,
-    get_discrete_events, get_defaults, get_systems, get_name, get_iv, get_gui_metadata, get_parent, get_systems
-using PlantGraphs, MultiScaleTreeGraph # Graphs
-using RecipesBase # Visualisation
+# # Imports
+using SciMLBase # interface with ODE solver packages
+using ModelingToolkit, Unitful # simulation
+import ModelingToolkit: get_eqs, get_unknowns, get_ps, get_parameter_dependencies, get_observed,
+    get_continuous_events, get_discrete_events, get_defaults, get_systems, get_name, get_iv,
+    get_gui_metadata, get_parent, get_systems # MTK internals
+using PlantGraphs, MultiScaleTreeGraph # graphs
+using RecipesBase # visualisation
 
+# # Include src files
 include("graph_nodetypes.jl")
 include("graph_functions.jl")
 include("graph_reading.jl")
 include("graph_conversion.jl")
 include("generate_system.jl")
 include("shapes.jl")
+include("smoothfuncs.jl")
 include("func_modules.jl")
 include("plotting.jl")
 
