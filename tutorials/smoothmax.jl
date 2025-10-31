44a838739bb67e0f88823762eb47a9e21c9e7106
Bram Spanoghe <70662421+bspanoghe@users.noreply.github.com>
2025-10-31 17:23:23 +0100

    pretty colors for figure thresholding

--- tutorials/smoothmax.jl
+++ tutorials/smoothmax.jl
@@ -1,25 +1,25 @@
-using Plots
-using Pkg; Pkg.activate("./tutorials")
-using LaTeXStrings
+using Plots, LaTeXStrings
 
 hardmax(x) = max(0, x)
 smoothmax(x; k) = x * 1/(1+exp(-k*x))
 LSE(x; α) = 1/α * log( sum( [exp(α * i) for i in [x, zero(x)]] ) )
 
-plot_font = "Computer Modern"
+# plot_font = "Computer Modern"
+include(homedir() * raw"\Documents\Github\Caverns_of_code\Julia\Lifehacks\catpuccin\get_palette.jl")
+cpalette = get_palette("prettycolors")
+plotdir = homedir() * raw"\Documents\Github\Den_of_evil\Non-note files\images\\"
 
 begin
     default()
-    default(fontfamily=plot_font,
-        linewidth=2, framestyle=nothing, label=nothing, grid=true)
+    default(linewidth=2)
     scalefontsizes(1.5)
 
     plot(title = "Comparison of thresholding functions", xlims = (-0.5, 1), 
         size = (800, 600), xlabel = L"x")
     plot!(hardmax, color = :black, label = L"\mathrm{max}(x, 0)", linewidth = 2)
-    plot!(x -> LSE(x, α = 5), color = RGB(1, 0.5, 0.5), linestyle = :dash, label = L"\mathrm{LSE}_5(x, 0)")
-    plot!(x -> LSE(x, α = 10), color = RGB(0.75, 0.5, 0.75), linestyle = :dash, label = L"\mathrm{LSE}_{10}(x, 0)")
-    plot!(x -> LSE(x, α = 50), color = RGB(0.5, 0.5, 1), linestyle = :dash, label = L"\mathrm{LSE}_{50}(x, 0)")
+    plot!(x -> LSE(x, α = 5), color = cpalette[1], linestyle = :dash, label = L"\mathrm{LSE}_5(x, 0)")
+    plot!(x -> LSE(x, α = 10), color = cpalette[2], linestyle = :dash, label = L"\mathrm{LSE}_{10}(x, 0)")
+    plot!(x -> LSE(x, α = 50), color = cpalette[3], linestyle = :dash, label = L"\mathrm{LSE}_{50}(x, 0)")
 end
 
-savefig("./tutorials/fig_plantmodules_max.pdf")
+savefig(plotdir * "fig_plantmodules_max.pdf")
