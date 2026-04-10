c914fc306d4a49a0ed9f30f5acd690b65d12153a
Bram Spanoghe <70662421+bspanoghe@users.noreply.github.com>
2026-04-10 17:14:10 +0200

    update imports of MTK internals

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,6 @@
 module tests
 using Test, PlantModules, PlantGraphs, ModelingToolkit, OrdinaryDiffEq, Plots
-import ModelingToolkit: get_eqs, get_systems, get_unknowns, get_defaults, get_name, get_iv
+import ModelingToolkit: get_eqs, get_systems, get_unknowns, get_name, get_iv
 
 @testset "Structure definition" include("./test_plantstructure.jl")
 @testset "System generation" include("./test_generate_system.jl")
